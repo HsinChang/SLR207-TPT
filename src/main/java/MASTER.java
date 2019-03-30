5e9ea766108d745d9a8f0246ceb89adfbced0c32
Hsin Chang <HsinChang@users.noreply.github.com>
2019-03-30 22:10:37 +0100

    End map phase

--- src/main/java/MASTER.java
+++ src/main/java/MASTER.java
@@ -6,76 +6,65 @@ public class MASTER {
     private static String userPrefix = "xizhang@";
     private static String domain = ".enst.fr";
     private static String splitsPath = "/tmp/xizhang/splits/";
+    private static String mapsPath = "/tmp/xizhang/maps";
     private static String slavePath = "/tmp/xizhang/slave.jar";
 
     private MASTER(){}
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         MASTER master = new MASTER();
         master.foo();
     }
 
-    private void foo() throws IOException {
-        ProcessBuilder pb;
+    private void foo() throws IOException, InterruptedException {
         BufferedReader reader;
-        Process p;
-        String tm, line;
-        int index;
+        String line;
 
         //target machines
         Map<String, Integer> targetMachines = new HashMap<>();
         targetMachines.put("c128-17", 0);
         targetMachines.put("c128-22", 1);
         targetMachines.put("c128-24", 2);
-        Map<String, List<String>> results = new HashMap<>();
 
+        // Define the MR map
+        Map<String, List<String>> keysUMxMap = new HashMap<>();
 
-        for (String machine : targetMachines.keySet()) {
-            tm = String.format("%s%s%s", userPrefix, machine, domain);
-            index = targetMachines.get(machine);
+        String[] machines = targetMachines.keySet().toArray(new String[targetMachines.keySet().size()]);
 
-            //Start connection
-            System.out.println("[BEGIN] Starting connection to machine " + tm);
-            pb = new ProcessBuilder("ssh", tm, "hostname");
-            pb.redirectErrorStream(true);
-            p = pb.start();
-            reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
-            line = reader.readLine();
-            if (!line.equals(machine)) {
-                System.out.println("[ERR] Can't connect to machine " + tm + " where l = " + line);
-                continue;
-            }
-            System.out.println("[OK] Connection available with machine " + tm);
-
-            //Create directory
-            pb = new ProcessBuilder("ssh", tm, "mkdir -p " + splitsPath);
-            pb.redirectErrorStream(true);
-            pb.start();
-            System.out.println("[OK] Created dir on machine " + tm);
-
-            //file copy
-            pb = new ProcessBuilder("scp", splitsPath + "S" + index +".txt",
-                    tm + ":" + splitsPath + "*" + "S" + index + ".txt");
-            pb.redirectErrorStream(true);
-            pb.start();
-            System.out.println("[OK] Copied targetFiles on machine " + tm);
-
-            //run slave
-            pb = new ProcessBuilder("java", "-jar", slavePath);
-            p = pb.start();
-            System.out.println("[OK] Launched slave.jar on machine " + tm);
-            // Receive the output from the Slave currently running, update the map
-            reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        ProcessBuilder[] mappersProcessBuilder = new ProcessBuilder[machines.length];
+        Process[] mappersProcess = new Process[machines.length];
+        for (int j = 0; j < machines.length; j++) {
+            mappersProcessBuilder[j] = new ProcessBuilder("scp", splitsPath + "S" + targetMachines.get(machines[j]) + ".txt ", machines[j] +":"+splitsPath);
+            mappersProcess[j] = mappersProcessBuilder[j].start();
+            System.out.println("[OK] file distributed on machine " + machines[j]);
+        }
+        for (int j = 0; j < machines.length; j++) {
+            mappersProcessBuilder[j] = new ProcessBuilder("ssh", machines[j], "java -jar " + slavePath + " 0 " + splitsPath + "S" + targetMachines.get(machines[j]) + ".txt");
+            mappersProcess[j] = mappersProcessBuilder[j].start();
+            System.out.println("[OK] Launched slave.jar MAP MODE on machine " + machines[j]);
+        }
+        // Wait for their completion and read the outputs
+        for (int j = 0; j < machines.length; j++) mappersProcess[j].waitFor();
+        // Receive the outputs of the mappers
+        for (int j = 0; j < machines.length; j++) {
+            reader = new BufferedReader(new InputStreamReader(mappersProcess[j].getInputStream()));
             while ((line = reader.readLine()) != null) {
-                if (results.containsKey(line)) {
-                    results.get(line).add("UM" + targetMachines.get(machine));
+                if (keysUMxMap.containsKey(line)) {
+                    keysUMxMap.get(line).add("UM" + targetMachines.get(machines[j]));
                 } else {
                     List<String> newList = new ArrayList<>();
-                    newList.add("UM" + targetMachines.get(machine));
-                    results.put(line, newList);
+                    newList.add("UM" + targetMachines.get(machines[j]));
+                    keysUMxMap.put(line, newList);
                 }
             }
         }
+        for (String r : keysUMxMap.keySet())
+            System.out.println(r + " - <" + keysUMxMap.get(r).toString() + ">");
+
+        //shuffle
+        
+
+
     }
     /*
     public static void main(String[] args) {
--- src/main/java/SLAVE.java
+++ src/main/java/SLAVE.java
@@ -1,6 +1,125 @@
+import java.util.*;
+import java.io.*;
+
 public class SLAVE {
-    public static void main(String[] args){
-        int a = 3 + 5;
-        System.out.println(a);
+    private static String rootFolder = "/tmp/xizhang/";
+    private int opCode;
+    private String fileToMap;
+    private String outputFile;
+    private String shuffleKey;
+    private String reduceKey;
+    private String reduceInput;
+
+    public SLAVE(String[] args) throws IOException{
+        this.opCode = Integer.parseInt(args[0]);
+        if (this.opCode == 0){
+            // Mode mapping
+            this.fileToMap = args[1];
+            this.map();
+        } else if (this.opCode == 1) {
+            this.shuffleKey = args[1];
+            this.outputFile = args[2];
+            this.shuffle(args);
+        } else if (this.opCode == 2) {
+            this.reduceKey = args[1];
+            this.reduceInput = args[2];
+            this.outputFile = args[3];
+            this.reduce();
+        } else {
+            System.err.println("Error: wrong operation code.");
+        }
     }
+
+    public static void main(String[] args) throws IOException {
+        if (args.length < 2){
+            System.out.println("[ERR] Wrong grammar, not enough arguments");
+            return;
+        }
+        new SLAVE(args);
+    }
+
+    private void map() {
+        System.out.println("[MAP]");
+        String[] items = fileToMap.split("/");
+        String numberOfFile = items[items.length-1].split("")[1];
+        String fileOutput = rootFolder+"maps/UM"+numberOfFile+".txt";
+        Set<String> keys = new HashSet<>();
+
+        BufferedReader reader;
+        BufferedWriter writer;
+        try {
+            reader = new BufferedReader(new FileReader(fileToMap));
+            writer = new BufferedWriter(new FileWriter(fileOutput));
+            String line = reader.readLine();
+
+            String[] splited = line.split(" ");
+            for (int i = 0; i < splited.length; ++i) {
+                keys.add(splited[i]);
+                writer.write(splited[i] + " 1\n");
+            }
+            writer.flush();
+            writer.close();
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Iterator<String> it = keys.iterator();
+        while (it.hasNext()) {
+            Object next = it.next();
+            System.out.println(next);
+        }
+    }
+
+    private void shuffle(String[] args) {
+        BufferedReader reader;
+        BufferedWriter writer;
+        try {
+            writer = new BufferedWriter(new FileWriter(outputFile));
+            for (int i = 3; i < args.length; ++i) {
+                reader = new BufferedReader(new FileReader(args[i]));
+
+                String line = reader.readLine();
+                while (line != null) {
+                    if (line.contains(shuffleKey)) {
+                        System.out.println(line);
+                        writer.write(line + "\n");
+                    }
+                    // read next line
+                    line = reader.readLine();
+                }
+                reader.close();
+
+            }
+            writer.flush();
+            writer.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void reduce() {
+        BufferedReader reader;
+        BufferedWriter writer;
+        int nKey = 0;
+        try {
+            writer = new BufferedWriter(new FileWriter(outputFile));
+            reader = new BufferedReader(new FileReader(reduceInput));
+
+            while (reader.readLine() != null)
+                nKey++;
+
+            writer.write(reduceKey + " " + nKey + "\n");
+
+            reader.close();
+            writer.flush();
+            writer.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
