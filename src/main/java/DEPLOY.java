e23083ad365f66cedc905f975a8b44d69a5d0fa8
Hsin Chang <HsinChang@users.noreply.github.com>
2019-03-30 17:56:53 +0100

    Create DEPLOY.java

---
+++ src/main/java/DEPLOY.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.ProcessBuilder;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class DEPLOY {
+
+	public static void main(String[] args) {
+		BufferedReader br = null, input = null;
+		List<String> machines = null;
+		ProcessBuilder pb = null;
+		Process p = null;
+		String l, slavePath = "/tmp/xizhang/", userPrefix = "xizhang@", slaveFile = "slave.jar", domain = ".enst.fr";
+
+		try {
+			br = new BufferedReader(new FileReader("./machines.txt"));
+			machines = new ArrayList<>();
+			String sCurrentLine;
+
+			while ((sCurrentLine = br.readLine()) != null) machines.add(userPrefix+sCurrentLine);
+
+			br.close();
+
+			for (String m : machines) {
+				// ProcessBuilder for checking connection with hostname
+				pb = new ProcessBuilder("ssh", m, "hostname");
+				pb.redirectErrorStream(true);
+				// Process
+				p = pb.start();
+				// Get output
+				input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+				l = userPrefix+input.readLine()+domain;
+				if (!l.equals(m)) {
+					System.out.println("[ERR] Can't connect to machine "+m);
+					continue;
+				}
+				System.out.println("[OK] Connection available with machine "+m);
+
+				// ProcessBuilder for mkdir
+				pb = new ProcessBuilder("ssh", m, "mkdir -p "+slavePath);
+				pb.redirectErrorStream(true);
+				// Process
+				p = pb.start();
+				System.out.println("[OK] Created dir on machine "+m);
+
+				// ProcessBuilder for copying slave to remote
+				pb = new ProcessBuilder("scp", slavePath+slaveFile, m+":"+slavePath);
+				pb.redirectErrorStream(true);
+				// Process
+				p = pb.start();
+				System.out.println("[OK] Copied slaveFile on machine "+m);
+
+			}		
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			// Clean the file openers
+			try {
+				if (br != null)
+					br.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+
+}
