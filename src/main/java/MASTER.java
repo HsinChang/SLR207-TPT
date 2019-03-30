10eaf503dcb843e649e9f28945984b75b41dfec5
Hsin Chang <HsinChang@users.noreply.github.com>
2019-03-30 18:31:51 +0100

    étape 41, MASTER split

--- src/main/java/MASTER.java
+++ src/main/java/MASTER.java
@@ -1,5 +1,5 @@
 import java.io.*;
-import java.util.concurrent.*;
+import java.util.*;
 
 public class MASTER {
 
@@ -8,6 +8,76 @@ public class MASTER {
     private static String splitsPath = "/tmp/xizhang/splits/";
     private static String slavePath = "/tmp/xizhang/slave.jar";
 
+    private MASTER(){}
+
+    public static void main(String[] args) throws IOException {
+        MASTER master = new MASTER();
+        master.foo();
+    }
+
+    private void foo() throws IOException {
+        ProcessBuilder pb;
+        BufferedReader reader;
+        Process p;
+        String tm, line;
+        int index;
+
+        //target machines
+        Map<String, Integer> targetMachines = new HashMap<>();
+        targetMachines.put("c128-17", 0);
+        targetMachines.put("c128-22", 1);
+        targetMachines.put("c128-24", 2);
+        Map<String, List<String>> results = new HashMap<>();
+
+
+        for (String machine : targetMachines.keySet()) {
+            tm = String.format("%s%s%s", userPrefix, machine, domain);
+            index = targetMachines.get(machine);
+
+            //Start connection
+            System.out.println("[BEGIN] Starting connection to machine " + tm);
+            pb = new ProcessBuilder("ssh", tm, "hostname");
+            pb.redirectErrorStream(true);
+            p = pb.start();
+            reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            line = reader.readLine();
+            if (!line.equals(machine)) {
+                System.out.println("[ERR] Can't connect to machine " + tm + " where l = " + line);
+                continue;
+            }
+            System.out.println("[OK] Connection available with machine " + tm);
+
+            //Create directory
+            pb = new ProcessBuilder("ssh", tm, "mkdir -p " + splitsPath);
+            pb.redirectErrorStream(true);
+            pb.start();
+            System.out.println("[OK] Created dir on machine " + tm);
+
+            //file copy
+            pb = new ProcessBuilder("scp", splitsPath + "S" + index +".txt",
+                    tm + ":" + splitsPath + "*" + "S" + index + ".txt");
+            pb.redirectErrorStream(true);
+            pb.start();
+            System.out.println("[OK] Copied targetFiles on machine " + tm);
+
+            //run slave
+            pb = new ProcessBuilder("java", "-jar", slavePath);
+            p = pb.start();
+            System.out.println("[OK] Launched slave.jar on machine " + tm);
+            // Receive the output from the Slave currently running, update the map
+            reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            while ((line = reader.readLine()) != null) {
+                if (results.containsKey(line)) {
+                    results.get(line).add("UM" + targetMachines.get(machine));
+                } else {
+                    List<String> newList = new ArrayList<>();
+                    newList.add("UM" + targetMachines.get(machine));
+                    results.put(line, newList);
+                }
+            }
+        }
+    }
+    /*
     public static void main(String[] args) {
         final LinkedBlockingQueue<String> lbq = new LinkedBlockingQueue<>();
 
@@ -50,4 +120,6 @@ public class MASTER {
 
 
     }
+
+     */
 }
